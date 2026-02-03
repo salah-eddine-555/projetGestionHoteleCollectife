b7ab05e7ff10c1aee718e7099a5f078a64e6d590
Aymane <aymanemj@proton.me>
2026-02-03 17:18:05 +0100

    Authentication form

--- GestionHotele/routes/web.php
+++ GestionHotele/routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('client.home');
 });
+
+Route::get('/login', function () {
+    return view('authentication.login');
+});
+
+Route::get('/register', function () {
+    return view('authentication.register');
+});
+
