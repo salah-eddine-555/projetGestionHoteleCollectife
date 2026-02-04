9f0e6df0bd7f28a5487826cf08881966a27a00bb
aitbhou2003 <abdellahaitbhou@gmail.com>
2026-02-04 14:32:00 +0100

    ajouter les rout de inscription

--- GestionHotele/routes/web.php
+++ GestionHotele/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,9 +11,9 @@ Route::get('/login', function () {
     return view('authentication.login');
 });
 
-Route::get('/register', function () {
-    return view('authentication.register');
-});
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
 
 Route::get('/admin/dashboard', function () {
     return view('admin.dashboard');
