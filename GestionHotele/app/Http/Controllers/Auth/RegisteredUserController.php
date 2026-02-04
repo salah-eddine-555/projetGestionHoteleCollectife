a4c035280184e71b9b9e153058d4efa6ee44846d
aitbhou2003 <abdellahaitbhou@gmail.com>
2026-02-04 14:26:58 +0100

    la fonction sorte pour ajouter un user apre l'inscription

--- GestionHotele/app/Http/Controllers/Auth/RegisteredUserController.php
+++ GestionHotele/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
 use PhpParser\Builder\Function_;
 use Psy\CodeCleaner\FunctionContextPass;
 
@@ -11,7 +15,35 @@ class RegisteredUserController extends Controller
 {
     //
 
-    public Function create(){
-        return view('auth.register');
+    public function create()
+    {
+        return view('authentication.register');
+    }
+
+    public function store(Request $request)
+    {
+
+        // dd($request->all());
+        //validate the request
+        $request->validate([
+            'firstname' => ['required', 'string', 'max:255'],
+            'lastname' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', 'min:8'],
+            'roles' => ['required']
+        ]);
+
+        User::create([
+            'firstname' => $request->firstname,
+            'lastname' => $request->lastname,
+            'email' => $request->email,
+            'password' => $request->password,
+            'role_id' => (int)$request->roles
+        ]);
+
+        //creat the use rin the database
+        //loh thim in
+        //redirect to the home
+
     }
 }
