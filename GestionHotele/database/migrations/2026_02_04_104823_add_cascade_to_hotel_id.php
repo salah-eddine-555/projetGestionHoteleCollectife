881702c0bd23ff5de3e6c73b4b3f36ae9809d7c7
Aymane <aymanemj@proton.me>
2026-02-05 12:13:13 +0100

    Fixed migration bugs

--- GestionHotele/database/migrations/2026_02_04_104823_add_cascade_to_hotel_id.php
+++ GestionHotele/database/migrations/2026_02_04_104823_add_cascade_to_hotel_id.php
@@ -11,12 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('chambres', function (Blueprint $table) {
+/*         Schema::table('chambres', function (Blueprint $table) {
+            $table->dropColumn('hotel_id');
             $table->foreign('hotel_id')
             ->references('id')
             ->on('hotels')
             ->onDelete('cascade');
-        });
+        }); */
     }
 
     /**
@@ -24,8 +25,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('chambres', function (Blueprint $table) {
+/*         Schema::table('chambres', function (Blueprint $table) {
             //
-        });
+        }); */
     }
 };
--- GestionHotele/database/migrations/2026_02_04_141004_create_roles_table.php
+++ GestionHotele/database/migrations/2026_02_04_141004_create_roles_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        /* Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-        });
+        }); */
     }
 
     /**
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
-    }
+/*         Schema::dropIfExists('roles');
+ */    }
 };
