27b7dea74af44c5041428d70f2f6c037b2c79f59
aitbhou2003 <abdellahaitbhou@gmail.com>
2026-02-05 09:33:52 +0100

    l'authentification

--- GestionHotele/routes/web.php
+++ GestionHotele/routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\Auth\SessionsController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
+use PharIo\Manifest\AuthorCollection;
 
 Route::get('/', [SiteController::class, 'index']);
 
 
-Route::get('/login', function () {
-    return view('authentication.login');
-});
+
 
 Route::get('/register', [RegisteredUserController::class, 'create']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
@@ -20,6 +21,9 @@ Route::get('/admin/dashboard', function () {
 });
 
 Route::get('/hotel/{id}', [SiteController::class, 'show']);
+Route::delete('/logout',[SessionsController::class,'destroy']);
+Route::get('/login',[SessionsController::class,'create']);
+Route::post('/login',[SessionsController::class,'store']);
 
 
 
