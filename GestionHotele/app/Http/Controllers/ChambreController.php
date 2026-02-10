d0814138d5108c32b2c58009c6e409dd32d60d43
Aymane-Elhassouni <aymane.elhassouni3@gmail.com>
2026-02-10 12:07:15 +0100

    ajoute crud chambre complete

--- GestionHotele/app/Http/Controllers/ChambreController.php
+++ GestionHotele/app/Http/Controllers/ChambreController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Chambre;
+use App\Models\Property;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class ChambreController extends Controller
@@ -10,10 +12,19 @@ class ChambreController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $chambres = Chambre::with('tags','proprties')->get();
-        return view('chambre.index',compact('chambres'));
+        $query = Chambre::with('tags', 'properties');
+        if ($tagId = $request->get('tag')) {
+            $query->whereHas('tags', fn($q) => $q->where('tags.id', $tagId));
+        }
+        if ($propertyId = $request->get('property')) {
+            $query->whereHas('properties', fn($q) => $q->where('properties.id',$propertyId));
+        }
+        $chambres = $query->get();
+        $allTags = Tag::all();
+        $allProperties = Property::all();
+        return view('client.home', compact('chambres', 'allTags','allProperties'));
     }
 
     /**
@@ -21,7 +32,7 @@ class ChambreController extends Controller
      */
     public function create()
     {
-        //
+        return view('manager.chambres.create');
     }
 
     /**
@@ -29,7 +40,17 @@ class ChambreController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+        'hotel_id' => 'required|integer',
+        'number' => 'required|string',
+        'price_per_night' => 'required|numeric|min:0',
+        'capacity' => 'required|integer|min:1',
+        'description' => 'nullable|string',
+        ]);
+        $chambre = Chambre::create($validated);
+        $chambre->tags()->sync($request->get('tags', []));
+        $chambre->properties()->sync($request->get('properties', []));
+        return redirect()->route('chambres.show', $chambre);
     }
 
     /**
@@ -37,31 +58,42 @@ class ChambreController extends Controller
      */
     public function show(Chambre $chambre)
     {
-        $chambre = Chambre::with('tags','proprties');
-        return view('chambre.index',compact('chambres'));
+        // $chambre = Chambre::with('tags','properties');
+        $chambre->load(['tags', 'properties']);
+        
+        return view('client.chambre-details',compact('chambre'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Chambre $chambre)
     {
-        //
+        return view('client.chambres.edit',compact('chambre'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Chambre $chambre)
     {
-        //
+        $validated = $request->validate([
+        'hotel_id' => 'required|integer',
+        'number' => 'required|string',
+        'price_per_night' => 'required|numeric|min:0',
+        'capacite' => 'required|integer|min:1',
+        'description' => 'nullable|string',
+        ]);
+        $chambre->update($validated);
+        return redirect()->route('chambres.index')->with('success', 'Post updated successfully.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Chambre $chambre)
     {
-        //
+        $chambre->delete();
+        return redirect()->route('chambres.index');
     }
 }
