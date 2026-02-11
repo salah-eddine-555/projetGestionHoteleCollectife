f8b14d9b1cacac2769817f18d568a19edaa7c837
Aymane <aymanemj@proton.me>
2026-02-11 02:04:37 +0100

    Failed attemt at implementing date filtering and reservation

--- GestionHotele/resources/views/client/chambres.blade.php
+++ GestionHotele/resources/views/client/chambres.blade.php
@@ -6,16 +6,62 @@
         <div class="container px-4 px-lg-5 my-5">
             <div class="text-center text-white">
                 @if (Auth::check())
-                    <p class="lead fw-normal text-white-50 mb-0">Welcome  {{ Auth::user()->firstname }}</p>
+                    <p class="lead fw-normal text-white-50 mb-0">Welcome {{ Auth::user()->firstname }}</p>
                 @endif
                 <h1 class="display-4 fw-bolder">Find your hostel</h1>
                 <p class="lead fw-normal text-white-50 mb-0">With StayEase</p>
-                
+
 
             </div>
         </div>
     </header>
     <!-- Section-->
+    <section class="py-5">
+        <div class="container px-4 px-lg-5 mt-5">
+            <form method="get" action="{{ route('chambres.index') }}">
+                @csrf
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <div class="form-floating mb-3">
+                            <input name="startDate" class="form-control dateInputStart" id="startDate" type="date"
+                                min="2026-02-09" />
+                            <label for="startDate">Start Date</label>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-floating mb-3">
+                            <input name="endDate" class="form-control dateInputEnd" id="startDate" type="date" />
+                            <label for="endDate">Start Date</label>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-6">
+                        <select class="form-control " name='tag'>
+                            <option value=''>Tous les tags</option>
+                            @foreach ($allTags as $tag)
+                                <option value='{{ $tag->id }}'>{{ $tag->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div class = "col-md-6">
+                        <select class="form-control " name='property'>
+                            <option value=''>Toutes les propriétés</option>
+                            @foreach ($allProperties as $prop)
+                                <option value='{{ $prop->id }}'>{{ $prop->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+
+                <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
+                    <button class="btn btn-primary">Filter</button>
+                </div>
+            </form>
+
+        </div>
+    </section>
+    
     <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
@@ -32,12 +78,12 @@
                                     <!-- Hotel name-->
                                     <h5 class="fw-bolder">{{ $chambre->hotel->name }}</h5>
                                     <!-- Hotel reviews-->
-                                    <div class="d-flex justify-content-center small text-warning mb-2">
-                                        <div >{{ $chambre->capacite }}</div>
+                                    <div class="d-flex justify-content-center small text-success mb-2">
+                                        <div>{{ $chambre->capacite }}</div>
                                     </div>
 
-                                    <div class="d-flex justify-content-center small text-warning mb-2">
-                                        <div >{{ $chambre->category->name }}</div>
+                                    <div class="d-flex justify-content-center small text-primary mb-2">
+                                        <div>{{ $chambre->category->name }}</div>
                                     </div>
 
                                 </div>
@@ -53,7 +99,7 @@
                 @endforeach
 
             </div>
-            
+            {{ $chambres->links() }}
         </div>
     </section>
 
